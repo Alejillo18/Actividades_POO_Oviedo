4529cf225d9f76650cdbec8ca0b221de5f69191a
Alejo Oviedo <alejoemmanueloviedo@gmail.com>
2026-04-07 12:17:02 -0300

    Actividad 06-04

---
+++ main.ts
@@ -0,0 +1,84 @@
+//Ejercicio 1
+console.log("Hola, Alejo");
+
+//Ejercicio 1.1
+const nombre = document.getElementById("nombre") as HTMLInputElement;
+console.log("Hola, " + nombre.value);
+
+
+
+//Ejercicio 2
+
+console.log(118 + 56);
+
+//Ejercicio 3:
+
+console.log(12345 + 67890);
+
+
+//Ejercicio 4:
+
+console.log(12*13);
+
+//Ejercicio 5:
+
+console.log(321 - 213);
+
+//Ejercicio 6:
+
+console.log(301 / 3);
+
+//Ejercicio 7:
+
+console.log(301 % 3);
+
+
+//Ejercicio 8:
+
+console.log(-2+3*5);
+
+//Ejercicio 9:
+
+console.log((20+5)%6);
+
+//Ejercicio 10:
+
+console.log(15+-5*6/10);
+
+//Ejercicio 11:
+
+console.log(2+10/5*2-7 %1);
+
+
+//Ejercicio 12:
+
+let n1: number = 20;
+let n2: number = 5;
+let n3 :number = 6;
+let resultado : number = ((n1-n2)%n3);
+console.log(resultado);
+
+
+//Ejercicio 13:
+
+n1= 15;
+n2 = -5;
+n3= 6;
+let resultado2 : number = (n1+n2*n3/10);
+console.log(resultado2);
+
+
+
+//Ejercicio 14:
+
+let n4 : number = 121;
+let n5 : number = 132
+let resultado3 : number = (n4 * n5);
+console.log(resultado3);
+
+
+//Ejercicio 15:
+let n6 : number = 285;
+let n7 : number = 1396;
+let resultado4 : number = (n6 + n7);
+console.log(resultado4);
